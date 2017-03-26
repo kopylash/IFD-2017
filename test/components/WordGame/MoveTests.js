bd7d13e84a5545dea5fb27b6f2cc824d9b427d07
Vladyslav Kopylash <vladik.kopilash@gmail.com>
2017-03-26 23:00:26 +0300

    introduced enzyme tests for Move and MoveList

---
+++ test/components/WordGame/MoveTests.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Move from '../../../src/components/WordGame/Move';
+
+describe('Move', () => {
+  it('exists', () => {
+    expect(shallow(<Move word={'baker'} matches={[0,1]}/>)).to.exist;
+  });
+
+  it('renders green letters', () => {
+    expect(shallow(<Move word={'baker'} matches={[0,1]}/>))
+      .to.contain(<span className="green">b</span>);
+  });
+
+  it('renders red letters', () => {
+    expect(shallow(<Move word={'baker'} matches={[]}/>))
+      .to.contain(<span className="red">b</span>);
+  });
+});
