e3fef2b8625a9dddf436450ed443d08fb6e164ac
Vladyslav Kopylash <vladik.kopilash@gmail.com>
2017-04-06 23:17:14 +0300

    constants file

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+'use strict';
+
+export const WORDS = ['paper', 'grill', 'basil', 'hinge', 'ruler'];
+
+export const GAME_TYPES = {WORD: 'WORD', NUMBER: 'NUMBER'};
+
+export const NUMBER_GAME_RESPONSES = {
+  WIN: 'WIN',
+  SMALL: 'SMALL',
+  BIG: 'BIG'
+};
